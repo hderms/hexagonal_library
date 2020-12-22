dba3fdc4cb5957a69674df8073de7b283bfa1973
Dermot Haughey <hderms@gmail.com>
2020-12-22 15:10:13 -0600

    add logging to client

--- src/client.rs
+++ src/client.rs
@@ -1,37 +1,47 @@
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::Request;
 
 use async_stream::stream;
-use blake3::Hasher;
-use futures_util::StreamExt;
 use hexagonal::file_library_client::FileLibraryClient;
-use hexagonal::{Ack, UploadFileRequest};
+use hexagonal::UploadFileRequest;
 use std::io::Read;
-use tokio::sync::mpsc;
+
+use log::{info, trace};
 
 pub mod hexagonal {
     tonic::include_proto!("hexagonal");
 }
+const ADDRESS: &str = "http://127.0.0.1:10000";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = FileLibraryClient::connect("http://127.0.0.1:10000").await?;
+    env_logger::init();
+
+    info!("Connecting to {}", ADDRESS);
+    let mut client = FileLibraryClient::connect(ADDRESS).await?;
+
+    trace!("Opening STDIN");
     let mut stdin = std::io::stdin();
 
-        let mut buffer = [0u8; 1024];
-        let outbound = stream! {
-            loop {
-                let bytes_read: usize = stdin.read(&mut buffer).unwrap();
-                if (bytes_read > 0) {
-                    yield UploadFileRequest{
-                        chunk: (&buffer[..bytes_read]).to_vec()
-                    };
-                } else {
-                    break;
-                }
+    let mut buffer = [0u8; 1024];
+    let outbound = stream! {
+        loop {
+            let bytes_read: usize = stdin.read(&mut buffer).unwrap();
+            trace!("Read {} bytes from STDIN", bytes_read);
+            if (bytes_read > 0) {
+                yield UploadFileRequest{
+                    chunk: (&buffer[..bytes_read]).to_vec()
+                };
+            } else {
+            trace!("End of STDIN");
+                break;
             }
-        };
+        }
+    };
 
-        let response = client.upload_file(Request::new(outbound)).await?;
-        println!("the hash is {}", response.into_inner().hash);
+    let response = client.upload_file(Request::new(outbound)).await?;
+    info!(
+        "the hash returned from the server is {}",
+        response.into_inner().hash
+    );
 
     Ok(())
 }
