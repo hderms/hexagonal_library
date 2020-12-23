d6ef34bdc644d93518c67f2ca14d86c6ab15e1db
Dermot Haughey <hderms@gmail.com>
2020-12-23 11:37:11 -0600

    save file in server

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,11 @@ use blake3::Hasher;
 use futures_util::StreamExt;
 use hexagonal::file_library_server::{FileLibrary, FileLibraryServer};
 use hexagonal::{Ack, GetImageChunk, GetImageRequest, UploadFileRequest};
+use std::{io::Write, thread::sleep};
+use tempfile::NamedTempFile;
 use tokio::sync::mpsc;
+mod directory;
+use directory::get_directory_from_hash;
 
 pub mod hexagonal {
     tonic::include_proto!("hexagonal");
@@ -34,10 +38,28 @@ impl FileLibrary for FileLibraryS {
         let mut stream: tonic::Streaming<UploadFileRequest> = request.into_inner();
 
         let mut hasher = Hasher::new();
+        let mut temp_file = NamedTempFile::new().unwrap();
         while let Some(chunk) = stream.next().await {
             let c: UploadFileRequest = chunk?;
             hasher.update(&c.chunk);
+            temp_file.write(&c.chunk).unwrap();
         }
+
+        temp_file.flush().unwrap();
+        let hash = hasher.finalize();
+
+        let directory_name = get_directory_from_hash(hash);
+
+        let prefix = Path::new("./tmp");
+        let directory_path = prefix.join(directory_name);
+        fs::create_dir(directory_path.clone()).unwrap();
+
+        let mut file_path = directory_path.clone();
+        file_path.push(hash.to_hex().to_string());
+
+        //rename the temporary file
+
+        fs::rename(temp_file.path(), file_path.clone()).unwrap();
 
         let reply = hexagonal::Ack {
             ok: true,
